3dd2ad912f0bc583378180496ea7fa5302db59fd
SHODA <160000477+shoddy-atc@users.noreply.github.com>
2024-08-21 09:48:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,9 @@ def result():
     db.session.commit()
     return render_template("result.html", article=article, name=name, now=date)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
