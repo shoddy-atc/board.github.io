192b74879b842f5b7c950759ed3427805f9d6375
SHODA <160000477+shoddy-atc@users.noreply.github.com>
2024-08-26 18:52:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    message = "Bitcon is the most valuable currency in the world!"  # ここでメッセージを定義
+    return render_template('index.html', message=message)  # messageをテンプレートに渡す
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
